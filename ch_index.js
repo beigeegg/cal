d7911a95d48140031d2d190dd735941e53e505b0
Pablaw <chulhwa7@gmail.com>
2021-10-01 21:35:22 +0900

    👉20211001 JS sturdy

--- ch_index.js
+++ ch_index.js
@@ -1,7 +1,17 @@
-console.log('abc')
+const display = document.querySelector('#display');
+
 let btn1 = document.querySelector('.btn1');
 let btn2 = document.querySelector('.btn2');
 let btn3 = document.querySelector('.btn3');
-console.log(btn1)
-btn1.addEventListener('click', function()
-{ href="github.com" })
+
+btn1.addEventListener('click', ev1)
+function ev1() 
+{display.innerText = btn1.innerText;}
+
+btn2.addEventListener('click', ev2)
+function ev2() 
+{display.innerText = btn2.innerText;}
+
+btn3.addEventListener('click', ev3)
+function ev3() 
+{display.innerText = btn3.innerText;}
