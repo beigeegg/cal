8dc62681d1e78fb1a57fb201bacac23fea4a07cc
dennis.md(김동훈)/dkt <dennis.md@kakaocorp.com>
2021-10-03 13:13:10 +0900

    👉 20211003 계산기오류수정

--- dh_index.js
+++ dh_index.js
@@ -4,29 +4,47 @@ const numbers = document.querySelectorAll(".num");
 const display = document.querySelector("#display");
 const answer = document.querySelector(".answer");
 const reset = document.querySelector(".ac");
+let tempNum = "";
 let temp = [];
+let isMinus = false;
 
 numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
-    temp.push(e.target.innerText);
-    display.innerText = e.target.innerText;
+    tempNum = tempNum + e.target.innerText;
+    if (display.innerText === "0") {
+      display.innerText = tempNum;
+    } else {
+      display.innerText = tempNum;
+    }
   });
 });
-plus.addEventListener("click", (event) => {
-  display.innerText = e.target.innerText;
-  temp[temp.length - 1] = temp[temp.length - 1] * 1;
-  console.log(temp);
+plus.addEventListener("click", (e) => {
+  temp.push(Number(tempNum));
+  tempNum = "";
+  display.innerText = display.innerText + " " + e.target.innerText;
+  isMinus = false;
 });
 minus.addEventListener("click", (e) => {
-  display.innerText = e.target.innerText;
-  temp[temp.length - 1] = temp[temp.length - 1] * -1;
+  temp.push(Number(tempNum));
+  tempNum = "";
+  display.innerText = display.innerText + " " + e.target.innerText;
+  isMinus = true;
 });
 reset.addEventListener("click", () => {
   display.innerText = 0;
+  tempNum = "";
   temp = [];
 });
 answer.addEventListener("click", () => {
-  display.innerText = temp.reduce((acc, cur) => {
+  if (isMinus) {
+    temp.push(Number(tempNum) * -1);
+  } else {
+    temp.push(Number(tempNum));
+  }
+
+  tempNum = temp.reduce((acc, cur) => {
     return Number(acc) + Number(cur);
   });
+  temp = [];
+  display.innerText = tempNum;
 });
