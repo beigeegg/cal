2799d6b60883367b3a34057db7a01dfdb6d0fd02
Pablaw <chulhwa7@gmail.com>
2021-10-07 18:00:32 +0900

    Thurs Practice

--- ch_index.js
+++ ch_index.js
@@ -1,17 +1,25 @@
+// const display = document.querySelector('#display');
+
+// let btn1 = document.querySelector('.btn1');
+// let btn2 = document.querySelector('.btn2');
+// let btn3 = document.querySelector('.btn3');
+
+// btn1.addEventListener('click', ev1)
+// function ev1() 
+// {display.innerText = btn1.innerText;}
+
+// btn2.addEventListener('click', ev2)
+// function ev2() 
+// {display.innerText = btn2.innerText;}
+
+// btn3.addEventListener('click', ev3)
+// function ev3() 
+// {display.innerText = btn3.innerText;}
+
+const numbers = documnet.querySelector('.numbers');
 const display = document.querySelector('#display');
 
-let btn1 = document.querySelector('.btn1');
-let btn2 = document.querySelector('.btn2');
-let btn3 = document.querySelector('.btn3');
-
-btn1.addEventListener('click', ev1)
-function ev1() 
-{display.innerText = btn1.innerText;}
-
-btn2.addEventListener('click', ev2)
-function ev2() 
-{display.innerText = btn2.innerText;}
-
-btn3.addEventListener('click', ev3)
-function ev3() 
-{display.innerText = btn3.innerText;}
+numbers.forEach(number) 
+    function.number {
+    number.addEventListener('click', ev) }
+    function.ev{display.innerText = numbers.innerText}
